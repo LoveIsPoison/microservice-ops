07f695efc77e74e851564d576ecbafda60c64ed4
qianjs <qianjs163@163.com>
2018-07-30 16:55:44 +0800

    test

--- ops-task/env.sh
+++ ops-task/env.sh
@@ -3,4 +3,4 @@
 sed -i 's/profiler.collector.ip=127.0.0.1/profiler.collector.ip='$(echo $profiler_collector_ip)'/g' /agent/pinpoint.config
 sed -i 's/profiler.sampling.rate=20/profiler.sampling.rate='$(echo $profiler_sampling_rate)'/g' /agent/pinpoint.config
 export JAVA_OPTS=$JAVA_OPTS$RANDOM
-java -javaagent:/agent/pinpoint-bootstrap-1.7.2.jar $JAVA_OPTS -jar ops-task.jar
+java -javaagent:/agent/pinpoint-bootstrap-1.7.2.jar $JAVA_OPTS -jar ops-task2.jar
