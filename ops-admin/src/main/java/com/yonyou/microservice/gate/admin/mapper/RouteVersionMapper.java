c788831eb5195dd9bc6664d44a0067d623899de2
haguui <1043398@qq.com>
2018-08-02 18:02:32 +0800

    网关支持服务多版本

---
+++ ops-admin/src/main/java/com/yonyou/microservice/gate/admin/mapper/RouteVersionMapper.java
@@ -0,0 +1,12 @@
+package com.yonyou.microservice.gate.admin.mapper;
+
+import com.yonyou.microservice.gate.admin.entity.RouteVersion;
+
+import tk.mybatis.mapper.common.Mapper;
+/**
+ * @author joy
+ */
+public interface RouteVersionMapper extends Mapper<RouteVersion> {
+
+
+}
