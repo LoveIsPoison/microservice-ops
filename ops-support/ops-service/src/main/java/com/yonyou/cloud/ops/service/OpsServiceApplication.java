448b37621a69d5617b3689080920ab6d531cc976
饼干大猪猪 <273655920@qq.com>
2018-08-23 16:15:46 +0800

    ops-service ↑ 2.0

--- ops-support/ops-service/src/main/java/com/yonyou/cloud/ops/service/OpsServiceApplication.java
+++ ops-support/ops-service/src/main/java/com/yonyou/cloud/ops/service/OpsServiceApplication.java
@@ -3,7 +3,7 @@ package com.yonyou.cloud.ops.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
 
