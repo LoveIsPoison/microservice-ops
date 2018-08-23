8fac7f0670dd26a808f8f9d0998a0b612c8631d3
饼干大猪猪 <273655920@qq.com>
2018-08-23 15:43:17 +0800

    ops-alert ↑ 2.0

--- ops-support/ops-alert/src/main/java/com/yonyou/cloud/ops/alert/ops/alert/feign/IUserService.java
+++ ops-support/ops-alert/src/main/java/com/yonyou/cloud/ops/alert/ops/alert/feign/IUserService.java
@@ -1,7 +1,7 @@
 package com.yonyou.cloud.ops.alert.ops.alert.feign;
 
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
